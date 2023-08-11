224bf64788e8f68e31973490c07655c94da5b8b7
saad484 <aiddisaad484@gmail.com>
2023-08-11 16:06:32 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+   * main - prints the alphabet in lowercase,
+    * followed by a new line
+     * Return: Always 0 (Success)
+      */
+int main(void)
+{
+		int nBten;
+
+			for (nBten = 0; nBten <= 9; nBten++)
+			{
+				printf("%d", nBten);
+			}
+			putchar('\n');
+			return (0);
+}
