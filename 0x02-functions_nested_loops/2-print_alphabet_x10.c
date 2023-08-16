47151a9adc9dfa95ae3e36bff3454105a19729c3
saad484 <aiddisaad484@gmail.com>
2023-08-16 11:48:21 -0700

    prints the alphabets x10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * prints the allphabtes x10 times
+ */
+void print_alphabet_x10(void)
+{
+int n;
+for(n = 0; n < 10; n++)
+_putchar(print_alphabet());
+}
+_putchar('\n');
+}
