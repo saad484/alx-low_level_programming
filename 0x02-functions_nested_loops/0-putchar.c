84140dfbb7c7891e18ce6b293610100cb43f385d
saad484 <aiddisaad484@gmail.com>
2023-08-16 07:08:44 -0700

    0_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,17 @@
  * main - prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');	
+return (0);
 }
