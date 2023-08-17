2f0e00b5e6ec6dcb7dba9804ff6fcc8a424dab7d
saad484 <aiddisaad484@gmail.com>
2023-08-17 14:32:49 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 
-while (i < 10)
+/**while (i < 10)
 {
-/*putchar('0' + i);*/
+putchar('0' + i);
 
 i++;
-}
+}*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
