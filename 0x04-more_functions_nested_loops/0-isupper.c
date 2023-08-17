e664fb340097aca35f5739e34419e75e1709a4ee
saad484 <aiddisaad484@gmail.com>
2023-08-17 02:55:44 -0700

    issupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * Checks for uppercase character
+ * Returns 1 if c is uppercase
+ * else return 0
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+_putchar("1");
+}
+else
+{
+_putchar("0");
+}
+}
