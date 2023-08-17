993cec9c94742a88710e0c655a83fbf00d0a2ce9
saad484 <aiddisaad484@gmail.com>
2023-08-17 14:39:47 -0700

    betty soon of **

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,20 +5,20 @@
 * Return: 0
 */
 
+
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
 
 i = 0;
 
 /*while (i < 10)*/
 /*{*/
-/*putchar('0' + i);*/
-
-/*i++;*/
+/*	putchar(i);*/
 /*}*/
-printf("Infinite loop avoided! \\o/\n");
 
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
