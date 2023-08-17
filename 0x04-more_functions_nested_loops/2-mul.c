a49012a992858c170fc40059e1bc617d72fb878f
saad484 <aiddisaad484@gmail.com>
2023-08-17 03:28:50 -0700

    mul two intger

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * mul - mulltiplies the integers
+ * @a: the intger to mullitple
+ * @b: the intger to multiple
+ *
+ * Return: the mulitple of two intger
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
