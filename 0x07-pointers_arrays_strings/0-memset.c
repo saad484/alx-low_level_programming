da3d75d025019474cb6d0dc492997b6071666c12
saad484 <aiddisaad484@gmail.com>
2023-08-29 22:16:51 +0100

    fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,13 +7,14 @@
  *
  *Return: changed array with new value for n bytes
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-    int i = 0;
-    for (; n > 0; i++)
-    {
-    s[i] = b;
-    n--;
-    }
-    return (s);
+int i = 0;
+for (; n > 0; i++)
+{
+s[i] = b;
+n--;
+}
+return (s);
 }
