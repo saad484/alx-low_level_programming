47c06a5e2b203c1d4de3bd0aefec84bc6fbae31c
saad484 <aiddisaad484@gmail.com>
2023-08-29 22:09:07 +0100

    fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -41,6 +41,6 @@ char buffer[98] = {0x00};
 simple_print_buffer(buffer, 98);
 _memset(buffer, 0x01, 95);
 printf("-------------------------------------------------\n");
-simple_print_buffer(buffer, 98);    
+simple_print_buffer(buffer, 98);
 return (0);
 }
