715b86994d8efae7965c549cfe54340893b7423e
saad484 <aiddisaad484@gmail.com>
2023-08-30 21:43:11 +0100

    print string in rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,15 @@
 
 void _print_rev_recursion(char *s)
 {
+static int first = 1;
 if (*s == '\0')
 {
+if (!first)
+_putchar(' ');
 _putchar('\n');
 return;
 }
 _print_rev_recursion(s + 1);
 _putchar(*s);
+first = 0;
 }
