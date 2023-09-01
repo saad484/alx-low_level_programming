53c3dcd205bcbd10f4604d3db8c9bf209452b0e0
saad484 <aiddisaad484@gmail.com>
2023-09-01 21:09:57 +0100

    print the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - return the name of the program
+* @argc: argc
+* @argv: argv
+* Return: 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+if (argc >= 1)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
