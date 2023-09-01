eb3e82faa4f83d07ce9d0e03c5ba860c8f3c7750
saad484 <aiddisaad484@gmail.com>
2023-09-01 22:11:06 +0100

    count the number of the arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - return the name of the program
+* @argc: argc
+* @argv: argv
+* Return: 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
