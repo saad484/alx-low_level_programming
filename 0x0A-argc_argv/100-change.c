dcb6fe2f479f4f77bfac04388c11945c08afbbc7
saad484 <aiddisaad484@gmail.com>
2023-09-02 02:06:08 +0100

    coins and cents

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,16 +13,16 @@
 
 int main(int argc, char *argv[])
 {
-int i;
+int cents;
 int coins[] = {25, 10, 5, 2, 1};
 int num_coins = 0;
-int cents = atoi(argv[1]);
-
+int i;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
+cents = atoi(argv[1]);
 if (cents < 0)
 {
 printf("0\n");
