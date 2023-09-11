48d18bc3c1c579c945ec15be98719cd8f5a80bbc
saad484 <aiddisaad484@gmail.com>
2023-09-12 00:10:57 +0100

    add the header file dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - struct dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/
+typedef struct dog
+{
+char *name;
+float *age;
+char *owner;
+} dog_t;
