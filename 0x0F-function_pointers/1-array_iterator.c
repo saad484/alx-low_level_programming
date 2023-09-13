7b98432d6103d686f72ef94325aed3d7ad4a8f52
saad484 <aiddisaad484@gmail.com>
2023-09-13 18:37:31 +0100

    prints the values of array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include<stddef.h>
+#include "function_pointers.h"
+/**
+* array_iterator - iterat an array
+* @array: array to iterat
+* @size: size of the array
+* @action: pointer to the function
+*
+* Return: no return
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+if (array && action)
+{
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,5 @@
 #define MAIN_H
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* MAIN_H */
