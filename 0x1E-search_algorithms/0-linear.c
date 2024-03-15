c90bd057ae4a59382402e9d1c76d8c747186d941
saad484 <aiddisaad484@gmail.com>
2024-03-15 02:29:20 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,15 +1,33 @@
 #include "search.h"
 #include <stdio.h>
 
+/**
+* linear_search - function search value form array
+* using lineare search algorithm
+*  @array: array to search form
+*  @value: value to search for
+*  @size: size of the array
+*  Return: the fisrt index of the match value in the array
+*/
+
 int linear_search(int *array, size_t size, int value)
 {
-    size_t  i;
-	for (i = 0; i <= size; i++)
+	size_t  i;
+	int tmp;
+
+	if (array != NULL)
 	{
-		if (array[i] == value)
+		for (i = 0; i <= size; i++)
 		{
-			return (i);
+			tmp = array[i];
+
+			printf("Value checked array[%lu] = [%d]\n", i, tmp);
+
+			if (tmp == value)
+			{
+				return (i);
+			}
 		}
-    }
-		return (-1);
+	}
+	return (-1);
 }
