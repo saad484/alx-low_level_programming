8ab5075c4b893742e2e2acb472e72649b4213d85
saad484 <aiddisaad484@gmail.com>
2024-03-16 02:21:00 +0000

    skiplists search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -47,4 +47,5 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif /* SEARCH_ALGOS_H */
