1ccd7b13a73301039b9d263547a6662755640dad
saad484 <aiddisaad484@gmail.com>
2024-03-16 02:07:54 +0000

    exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,6 @@ int binary_search(int *array, size_t size, int value);
 int _binary_search(int *array, size_t left, size_t right, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
